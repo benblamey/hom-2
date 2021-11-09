5b1638861ce064bfe3da64a9d9c0342f43c438eb
Ben Blamey <blamey.ben@gmail.com>
2021-11-09 12:23:16 +0100

    created manager image, able to get and create pods from within manager pod

---
+++ manager/src/main/java/com/benblamey/hom/manager/Manager.java
@@ -0,0 +1,4 @@
+package com.benblamey.hom.manager;
+
+public class Manager {
+}
---
+++ manager/src/main/java/com/benblamey/hom/manager/ManagerMain.java
@@ -0,0 +1,10 @@
+package com.benblamey.hom.manager;
+
+public class ManagerMain {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
