bc6bca10ee5606d2c596a9f03f2fe9e6bc38c285
Ben Blamey <blamey.ben@gmail.com>
2022-06-03 00:07:31 +0200

    Create ubuntu-curl-install.sh

---
+++ ubuntu-curl-install.sh
@@ -0,0 +1,27 @@
+# Connect to the server forwarding ports to these ports on the server:
+#  localhost:10443 (for the microK8s dash)
+#  localhost:80 (for the HTTP services)
+# See: https://www.ibm.com/support/pages/what-are-ssh-tunnels-and-how-use-them
+
+sudo apt update ; sudo apt upgrade ; sudo snap install microk8s --classic ; sudo microk8s enable dns ingress
+
+# Access the Kubernetes admin dashboard (this keeps running so recommended open in a new session, or use &). Binds to https://127.0.0.1:10443
+sudo microk8s dashboard-proxy &
+
+sudo apt -y install git ; git clone https://github.com/HASTE-project/hom-2.git
+
+sudo microk8s kubectl create namespace hom ; sudo microk8s kubectl config set-context --current --namespace=hom
+
+# modify the persistent volume to match the current machine (check the host and path)
+
+# or attempt this with sed..
+sed -i s/hom-2-benblamey/$(hostname)/ hom-2/kubernetes/k8.yaml
+sed -i s+/home/ubuntu/mnt+$(pwd)+ hom-2/kubernetes/k8.yaml
+
+sudo microk8s kubectl apply -f hom-2/kubernetes/k8.yaml
+
+# See if everything is running:
+sudo microk8s kubectl get all --all-namespaces
+
+# port forward the web ingress to localhost (in the background)
+sudo microk8s kubectl port-forward --namespace=ingress daemonset.apps/nginx-ingress-microk8s-controller 80:80 &
