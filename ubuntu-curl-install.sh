e4367d10e38890045215ce9dcf20a7150bb942b9
Ben Blamey <blamey.ben@gmail.com>
2022-06-16 16:39:52 +0200

    install specific version of mikrok8s

--- ubuntu-curl-install.sh
+++ ubuntu-curl-install.sh
@@ -4,7 +4,7 @@
 # See: https://www.ibm.com/support/pages/what-are-ssh-tunnels-and-how-use-them
 
 
-sudo apt update ; sudo apt upgrade -y ; sudo snap install microk8s --classic ; sudo microk8s enable dns ingress rbac
+sudo apt update ; sudo apt upgrade -y ; sudo snap install microk8s --classic --channel=1.21/stable ; sudo microk8s enable dns ingress rbac
 
 # Access the Kubernetes admin dashboard (this keeps running so recommended open in a new session, or use &). Binds to https://127.0.0.1:10443
 sudo microk8s dashboard-proxy &
