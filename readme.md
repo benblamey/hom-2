225a93baa74fd4b7ba709205985ca1eb8777feec
Ben Blamey <blamey.ben@gmail.com>
2022-06-20 11:44:16 +0200

    fix multiple issues with dashboard deployment

--- readme.md
+++ readme.md
@@ -8,24 +8,40 @@ Part of the HASTE Project. [http://haste.research.it.uu.se/]()
 ## DEPLOYMENT INSTRUCTIONS FOR UBUNTU (21.04)
 
 0. Connect to a fresh VM, forwarding ports to these ports on the server:
-  localhost:10443 (for the microK8s dash)
-  localhost:80 (for the HTTP services)
+  localhost:8001 (for the microK8s dashboard)
+  localhost:80 (for the HOM HTTP services)
 See: [https://www.ibm.com/support/pages/what-are-ssh-tunnels-and-how-use-them]()
+
+For example:
+```
+sudo ssh ubuntu@<host> -i <key-file> -L 80:localhost:80 -L 8001:localhost:8001
+```
 
 1. Run the install script via curl (or copy-paste the commands [from the script](ubuntu-curl-install.sh))
 ```
 source <(curl -s https://raw.githubusercontent.com/HASTE-project/hom-2/main/ubuntu-curl-install.sh)
 ```
 
-The token used to access the dashboard is printed in the console.
+The token used to access the dashboard is printed in the console, it will look something like this:
+
+```
+--- BEGIN DASHBOARD TOKEN ---
+eyJhbGciOiJSUzI1NiIsImtpZCI6IjFCdTBGNXlCV3V2djZyb0xvRWtUU193WnBxZGVZcjRaOWVhcFpJbkFsMk
+EifQeyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZRlcy5pby9zZXJ2aWNlYWNjb3V
+udC9uYW1lc3BhY2UiOiJrdWJlLXN5c3RlbSIsImt1YmVybmV0ZXtH_CDT9TuTq3QntWw8_KbrbuUx6Qwteb8qL
+1wj7do9kGz1HH-EKBPt39OpmyEFJtYLeLANF6s0xngJgU3Th8aly0DaLTh674brY5jciKqkiulSMFtK3SxUYux
+aRPM-tgpFtHYAtgT8rk4q3dPbTRtJM5MPnsyaog96m6jTe8FX-e19Ad-OkVoJgdOWzN73iV9VI0WbrtYw
+--- END DASHBOARD TOKEN ---
+```
 
 2. Assuming port forwarding is setup correctly, you can now access (don't forget the trailing slash):
+
+[The Dashboard](http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/workloads?namespace=hom)
 
 [http://localhost/gui/](http://localhost/gui/) (the GUI)
 
 [http://localhost/notebook/](http://localhost/notebook/) (Jupyter) 
 
-[http://localhost:10443/](http://localhost:10443/) (the K8s dashboard)
 
 The password is `hej-hom-impl-foo` (Note that access to all the web services, including the notebook, is protected by the SSH login)
 
--- ubuntu-curl-install.sh
+++ ubuntu-curl-install.sh
@@ -3,29 +3,49 @@
 #  localhost:80 (for the HTTP services)
 # See: https://www.ibm.com/support/pages/what-are-ssh-tunnels-and-how-use-them
 
+# Tested on Ubuntu 20.04
 
-sudo apt update ; sudo apt upgrade -y ; sudo snap install microk8s --classic --channel=1.21/stable ; sudo microk8s enable dns ingress rbac
-
-# Access the Kubernetes admin dashboard (this keeps running so recommended open in a new session, or use &). Binds to https://127.0.0.1:10443
-sudo microk8s dashboard-proxy &
+sudo apt update -y ; sudo apt upgrade -y ; sudo snap install microk8s --classic --channel=1.24/stable ; sudo microk8s enable dns ingress rbac
 
 sudo apt -y install git ; git clone https://github.com/HASTE-project/hom-2.git
+
+# Deploy the dashboard
+# https://kubernetes.io/docs/tasks/access-application-cluster/web-ui-dashboard/
+sudo microk8s kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml
+
+# Create a sample user for the dashboard
+# https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md
+sudo microk8s kubectl apply -f hom-2/kubernetes/dashboard-admin.yaml
+
+# Create a print a token for the dashboard user
+echo "--- BEGIN DASHBOARD TOKEN ---"
+sudo microk8s kubectl -n kubernetes-dashboard create token admin-user
+echo "--- END DASHBOARD TOKEN ---"
+
+# Proxy to the dashboard
+sudo microk8s kubectl proxy &
+
 
 sudo microk8s kubectl create namespace hom ; sudo microk8s kubectl config set-context --current --namespace=hom
 
 # modify the persistent volume to match the current machine (check the host and path)
-
-# or attempt this with sed..
+# attempt this with sed..
 sed -i s/hom-2-benblamey/$(hostname)/ hom-2/kubernetes/storage.yaml
 sed -i s+/home/ubuntu/mnt+$(pwd)+ hom-2/kubernetes/storage.yaml
 sudo microk8s kubectl apply -f hom-2/kubernetes/storage.yaml
 
+# deploy the remaining resources
 sudo microk8s kubectl apply -f hom-2/kubernetes/k8.yaml
 
 # See if everything is running:
 sudo microk8s kubectl get all --all-namespaces
 
-# port forward the web ingress to localhost (in the background)
-sudo microk8s kubectl port-forward --namespace=ingress daemonset.apps/nginx-ingress-microk8s-controller 80:80 &
+# port forward the web ingress to localhost (in the background):
+# For for ubuntu+microK8s:
+sudo microk8s kubectl port-forward --namespace=ingress daemonset.apps/nginx-ingress-microk8s-controller 80:80
+# For Docker on a Mac:
+# sudo kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 80:80
+# For something else ?!:
+# sudo microk8s kubectl port-forward web-ingress 80:80 &
 
 echo -- HOM is Ready! --
